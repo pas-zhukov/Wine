a7a1f4cb46c9b1eff8ce0d26efda6547bfe0e3b0
Pavel <pas-zhukov@yandex.ru>
2023-06-05 23:53:18 +0300

    - unused files removed, added to gitignore - ReadMe.md:     - Added a template for input data (products.xlsx) - foundation date changed to year as int value - some variables and function names fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pandas import read_excel
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-WINERY_FOUNDATION_DATE = date(1920, 1, 1)
+WINERY_FOUNDATION_YEAR = 1920
 
 
 def adjust_year_sign(years: int) -> str:
@@ -17,8 +17,8 @@ def adjust_year_sign(years: int) -> str:
 
     if not isinstance(years, int) or years <= 0:
         raise ValueError("Year must be a positive integer!")
-    years = str(years)
 
+    years = str(years)  # Необходимо для работы с регулярными выражениями.
     if re.match(r"^(?!.*11$).*1$", years):
         return "год"
     if re.match(r"^(?!.*1[2-4]$)\d*[2-4]$", years):
@@ -27,19 +27,19 @@ def adjust_year_sign(years: int) -> str:
         return "лет"
 
 
-def read_products_info(path: str = "wine3.xlsx") -> dict[str, list[dict[str, str]]]:
+def read_products_xlsx(path: str = "products.xlsx") -> dict[str, list[dict[str, str]]]:
     """
     Reads product information from an Excel file and returns a dictionary where the keys are product categories and
     the values are lists of dictionaries representing the products in that category.
 
     """
 
-    data = read_excel(path, keep_default_na=False)
-    dict_data = collections.defaultdict(list)
-    for i, row in data.iterrows():
-        dict_data[row["Категория"]].append(row.drop("Категория").to_dict())
+    products_table_df = read_excel(path, keep_default_na=False)
+    products_by_category = collections.defaultdict(list)
+    for _, row in products_table_df.iterrows():
+        products_by_category[row["Категория"]].append(row.drop("Категория").to_dict())
 
-    return dict_data
+    return products_by_category
 
 
 if __name__ == "__main__":
@@ -48,12 +48,12 @@ if __name__ == "__main__":
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    winery_age = date.today().year - WINERY_FOUNDATION_DATE.year
+    winery_age = date.today().year - WINERY_FOUNDATION_YEAR
     template = env.get_template('template.html')
     rendered_page = template.render(
         winery_age=winery_age,
         year_sign=adjust_year_sign(winery_age),
-        products_data=read_products_info()
+        product_cards=read_products_xlsx()
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
